99157bba2c5c2e859fd1b31af5bb4d77f2a10154
Ethan Jurman <ethanjurman@gmail.com>
2024-01-06 12:13:47 -0500

    remove meta from git

--- meta.js
+++
@@ -1,5 +0,0 @@
-const client = contentful.createClient({
-  space: "j7c5ok9im263",
-  environment: "master", // defaults to 'master' if not set
-  accessToken: "KutzhikiE1FrfcvYPrQbKXjCZfnx8LyMrNl8RWGFfpE",
-});
